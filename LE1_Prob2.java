5c339503a7c8c9a7b15fa41b0823705dba52c506
Ayush singh <AYUSHSINGH2632003@GMAIL.COM>
2023-05-09 21:43:33 +0530

    Add files via upload

---
+++ LE1_Prob2.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.io.*;
+public class Main
+{
+    public static void printTriangle(int n)
+    {
+        for (int i=0; i<n; i++)
+        {
+            for (int j=n-i; j>1; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int j=0; j<=i; j++ )
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        int n = 4;
+        printTriangle(n);
+    }
+}
+		
